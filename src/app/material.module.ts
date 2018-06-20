56fe352fd2710958f24506dc1ed157938ce3a13f
Mikhail_Miroliubov <Mikhail_Miroliubov@epam.com>
2018-06-20 19:00:56 +0300

    MessagerComponent in progress

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,10 +1,10 @@
 import {NgModule} from "@angular/core";
-import {MatButtonModule, MatCardModule, MatFormFieldModule, MatInputModule} from "@angular/material";
+import {MatButtonModule, MatCardModule, MatDividerModule, MatFormFieldModule, MatInputModule} from "@angular/material";
 import {FlexLayoutModule} from "@angular/flex-layout";
 
 @NgModule({
-    imports: [ MatButtonModule, MatInputModule, MatFormFieldModule, MatCardModule, FlexLayoutModule ],
-    exports: [ MatButtonModule, MatInputModule, MatFormFieldModule, MatCardModule, FlexLayoutModule ]
+    imports: [ MatButtonModule, MatInputModule, MatFormFieldModule, MatCardModule, FlexLayoutModule, MatDividerModule ],
+    exports: [ MatButtonModule, MatInputModule, MatFormFieldModule, MatCardModule, FlexLayoutModule, MatDividerModule ]
 })
 export class MaterialAppModule {
 
--- src/app/messenger/messenger.component.ts
+++ src/app/messenger/messenger.component.ts
@@ -2,7 +2,8 @@ import {Component, OnInit} from "@angular/core";
 
 @Component({
     selector: 'app-messenger',
-    template: require('./messenger.component.html')
+    template: require('./messenger.component.html'),
+    styles:   [ require('./messenger.component.css') ]
 })
 export class MessengerComponent implements OnInit {
     constructor() {
