d12b8226ea6bf5f6f5e61bf75ff439a9e48d078e
Mikhail_Miroliubov <Mikhail_Miroliubov@epam.com>
2018-07-06 18:05:21 +0300

    MessageComponent in progress

--- src/app/messenger-module/dialog/message/message.component.ts
+++ src/app/messenger-module/dialog/message/message.component.ts
@@ -3,7 +3,8 @@ import {Message} from "../../model/Dialog";
 
 @Component({
   selector: 'app-message',
-  template: require('./message.component.html')
+  template: require('./message.component.html'),
+  styles:   [ require('./message.component.css') ]
 })
 export class MessageComponent implements OnInit {
   @Input() message: Message;
